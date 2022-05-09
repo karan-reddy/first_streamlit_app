5437ad647c597a288bb654b3a90cd09a807af630
karan-reddy <105191126+karan-reddy@users.noreply.github.com>
2022-05-09 13:33:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,4 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.title('My Mom\'s New Healthy Dinner')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+
 streamlit.title('My Mon\'s New Healthy Diner')
